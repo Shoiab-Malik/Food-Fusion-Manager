1cbcb9369e36119a0309d029a99ad8edc0dd4ad7
SHOIAB MALIK <khans766214@gmail.com>
2025-01-12 17:06:39 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+class ItemList(models.Model):
+    Category_name = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.Category_name
+
+class Items(models.Model):
+    Item_name = models.CharField(max_length=45)
+    description = models.TextField(blank=False)
+    Price = models.IntegerField()
+    Category = models.ForeignKey(ItemList, related_name='Name', on_delete=models.CASCADE)
+    Image = models.ImageField(upload_to='Items/')
+    
+    def __str__(self):
+        return self.Item_name
+
+class AboutUs(models.Model):
+    Description = models.TextField(blank=False)
+
+class BookTable(models.Model):
+    Name = models.CharField(max_length=15)
+    Phone_number = models.IntegerField()
+    Email = models.EmailField()
+    Total_person = models.IntegerField()
+    Booking_date = models.DateField()
+
+    def __str__(self):
+        return self.Name
